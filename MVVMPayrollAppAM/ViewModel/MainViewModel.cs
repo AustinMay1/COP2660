5faa7c7af4dc8fa71cc71ead2938046c9fae0503
AustinMay1 <austin.may@protonmail.com>
2023-10-19 10:17:35 -0400

    MVVMPayrollApp: implemented button commands + display output

--- MVVMPayrollAppAM/ViewModel/MainViewModel.cs
+++ MVVMPayrollAppAM/ViewModel/MainViewModel.cs
@@ -10,11 +10,39 @@ public partial class MainViewModel
     [ObservableProperty] private string _employeeName;
     [ObservableProperty] private float _employeeHours;
     [ObservableProperty] private float _employeePayRate;
-    [ObservableProperty] private string _payOutput = String.Empty;
+    [ObservableProperty] private string _payOutput = string.Empty;
 
     [RelayCommand]
     private void CalculatePay()
     {
-        PayOutput = "Hello, World";
+        const float taxRate = 0.2f;
+        var grossPay = EmployeeHours * EmployeePayRate;
+        var taxes = grossPay * taxRate;
+        var netPay = grossPay - taxes;
+
+        PayOutput +=
+            $"Employee Pay:\n" +
+            $"Employee ID: {EmployeeID}\n" +
+            $"Employee Name: {EmployeeName}\n" +
+            $"Employee Hours: {EmployeeHours}\n" +
+            $"Employee Pay Rate: {EmployeePayRate}\n" +
+            $"Gross Pay: ${grossPay}\n" +
+            $"Taxes: ${taxes}\n" +
+            $"Net Pay: ${netPay}\n\n";
+    }
+
+    [RelayCommand]
+    private void ClearInput()
+    {
+        EmployeeID = string.Empty;
+        EmployeeName = string.Empty;
+        EmployeeHours = 0;
+        EmployeePayRate = 0;
+    }
+
+    [RelayCommand]
+    private void ClearOutput()
+    {
+        PayOutput = string.Empty;
     }
 }
