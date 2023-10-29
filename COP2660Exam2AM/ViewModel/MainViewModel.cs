511c985267ca6833f56f73fb14072b773152e186
AustinMay1 <austin.may@protonmail.com>
2023-10-29 17:06:46 -0400

    Exam2: add error checking

--- COP2660Exam2AM/ViewModel/MainViewModel.cs
+++ COP2660Exam2AM/ViewModel/MainViewModel.cs
@@ -11,31 +11,47 @@ namespace COP2660Exam2AM.ViewModel;
 [ObservableObject]
 public partial class MainViewModel
 {
-    [ObservableProperty] private double _investmentValue;
-    [ObservableProperty] private double _interestRate;
-    [ObservableProperty] private int _compoundRate;
-    [ObservableProperty] private int _yearsInvested;
+    [ObservableProperty] private string _investmentValue = string.Empty;
+    [ObservableProperty] private string _interestRate = string.Empty;
+    [ObservableProperty] private string _compoundRate = string.Empty;
+    [ObservableProperty] private string _yearsInvested = string.Empty;
     [ObservableProperty] private string _output = string.Empty;
+    private readonly Page _mainPage = Application.Current?.MainPage;
 
-    private void _displayOutput(double fv)
+    private void _displayOutput(double fv, double p)
     {
-        Output += $"ROI: {fv:C2}";
+        var ai = fv - p;
+        Output = $"Future Value: {fv:C2}\nAccrued Interest {ai:C2}\nPrincipal: {p:C2}";
     }
     
     [RelayCommand]
-    private void CalculateInvestment()
+    private async Task CalculateInvestment()
     {
-        var fv = InvestmentValue * Math.Pow((1 + InterestRate / 100 / CompoundRate), CompoundRate * YearsInvested);
-        _displayOutput(fv: fv);
+        var p = string.IsNullOrEmpty(InvestmentValue) ? 0 : double.Parse(InvestmentValue);
+        var r = string.IsNullOrEmpty(InterestRate) ? 0 : double.Parse(InterestRate);
+        var n = string.IsNullOrEmpty(CompoundRate) ? 0 : double.Parse(CompoundRate);
+        var t = string.IsNullOrEmpty(YearsInvested) ? 0 : double.Parse(YearsInvested);
+        
+        if (p <= 0 || r <= 0 || n <= 0 || t <= 0 )
+        {
+            await _mainPage.DisplayAlert(
+                "Invalid/Empty Fields",
+                "One or more of your fields contain empty or invalid values. Please double check your fields and resubmit.",
+                "OK"
+                );
+            return;
+        }
+        var fv = p * Math.Pow((1 + r / 100 / n), n * t);
+        _displayOutput(fv: fv, p: p);
     }
 
     [RelayCommand]
     private void ClearForm()
     {
-        InvestmentValue = 0;
-        InterestRate = 0;
-        CompoundRate = 0;
-        YearsInvested = 0;
+        InvestmentValue = string.Empty;
+        InterestRate = string.Empty;
+        CompoundRate = string.Empty;
+        YearsInvested = string.Empty;
     }
 
     [RelayCommand]
