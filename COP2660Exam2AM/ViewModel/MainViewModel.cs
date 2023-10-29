80d6bbc0a8b1ee5e5a610d5001b8ca9450b97e87
AustinMay1 <austin.may@protonmail.com>
2023-10-29 14:05:47 -0400

    Exam2: calculate roi and display. implement clear commands

--- COP2660Exam2AM/ViewModel/MainViewModel.cs
+++ COP2660Exam2AM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,37 @@ namespace COP2660Exam2AM.ViewModel;
 [ObservableObject]
 public partial class MainViewModel
 {
-    [ObservableProperty] private double _initialInvestment;
+    [ObservableProperty] private double _investmentValue;
     [ObservableProperty] private double _interestRate;
     [ObservableProperty] private int _compoundRate;
     [ObservableProperty] private int _yearsInvested;
+    [ObservableProperty] private string _output = string.Empty;
+
+    private void _displayOutput(double fv)
+    {
+        Output += $"ROI: {fv:C2}";
+    }
     
+    [RelayCommand]
+    private void CalculateInvestment()
+    {
+        var fv = InvestmentValue * Math.Pow((1 + InterestRate / 100 / CompoundRate), CompoundRate * YearsInvested);
+        _displayOutput(fv: fv);
+    }
+
+    [RelayCommand]
+    private void ClearForm()
+    {
+        InvestmentValue = 0;
+        InterestRate = 0;
+        CompoundRate = 0;
+        YearsInvested = 0;
+    }
+
+    [RelayCommand]
+    private void ClearAll()
+    {
+        ClearForm();
+        Output = string.Empty;
+    }
 }
